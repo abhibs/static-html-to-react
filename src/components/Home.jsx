2242789dcbf98f363d57e9e629f62a8db39cc5d1
abhibs <abhirambs97@gmail.com>
2025-02-06 12:53:48 +0530

    one slider commented

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,7 @@ class Home extends Component {
                         <div class="hero__caption">
                             <span data-animation="fadeInUp" data-delay=".4s">Get Every Single Solutions.</span>
                             <h1 data-animation="fadeInUp" data-delay=".6s">I’m Designer Haris F. Watson</h1>
-                            <P data-animation="fadeInUp" data-delay=".8s" >jhorem rfpsum golor sidt amet, consectetur adipiscing elit, eiusmod tempor incididunt utcjhg labore bet dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.</P>
+                            <p data-animation="fadeInUp" data-delay=".8s" >jhorem rfpsum golor sidt amet, consectetur adipiscing elit, eiusmod tempor incididunt utcjhg labore bet dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.</p>
                             <div class="hero__btn">
                                 <a href="industries.html" class="btn hero-btn"  data-animation="fadeInLeft" data-delay=".8s">Learn More</a>
                                 <a href="industries.html" class="btn border-btn ml-15" data-animation="fadeInRight" data-delay="1.0s">Hire Me</a>
@@ -25,14 +25,14 @@ class Home extends Component {
                 </div>
             </div>
         </div>
-        <div class="single-slider slider-height d-flex align-items-center">
+        {/* <div class="single-slider slider-height d-flex align-items-center">
             <div class="container">
                 <div class="row">
                     <div class="col-xl-6 col-lg-6 col-md-8">
                         <div class="hero__caption">
                             <span data-animation="fadeInUp" data-delay=".4s">Get Every Single Solutions.</span>
                             <h1 data-animation="fadeInUp" data-delay=".6s">I’m Designer Haris F. Watson</h1>
-                            <P data-animation="fadeInUp" data-delay=".8s" >jhorem rfpsum golor sidt amet, consectetur adipiscing elit, eiusmod tempor incididunt utcjhg labore bet dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.</P>
+                            <p data-animation="fadeInUp" data-delay=".8s" >jhorem rfpsum golor sidt amet, consectetur adipiscing elit, eiusmod tempor incididunt utcjhg labore bet dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.</p>
                             <div class="hero__btn">
                                 <a href="industries.html" class="btn hero-btn"  data-animation="fadeInLeft" data-delay=".8s">Learn More</a>
                                 <a href="industries.html" class="btn border-btn ml-15" data-animation="fadeInRight" data-delay="1.0s">Hire Me</a>
@@ -41,7 +41,7 @@ class Home extends Component {
                     </div>
                 </div>
             </div>
-        </div>
+        </div> */}
     </div>
 </div>
 <section class="about-area section-paddingt30">
