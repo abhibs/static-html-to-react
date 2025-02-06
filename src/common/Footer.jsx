ea54fe7c2a893d2519747edb56cc86d191df3964
abhibs <abhirambs97@gmail.com>
2025-02-06 11:46:11 +0530

    comment line removed and closing tag added to image

--- src/common/Footer.jsx
+++ src/common/Footer.jsx
@@ -5,14 +5,13 @@ export default class Footer extends Component {
     return (
       <div>
             <footer>
-        <!-- Footer Start-->
         <div class="footer-area">
             <div class="container">
                <div class="footer-top footer-padding">
                    <div class="row justify-content-center">
                        <div class="col-lg-6">
                             <div class="footer-top-cap text-center">
-                                <img src="assets/img/logo/logo2_footer.png" alt="">
+                                <img src="assets/img/logo/logo2_footer.png" alt=""/>
                                 <span><a href="#">hireme@portfolio.com</a></span>
                                 <p>221B Baker Street, Post office Box 353 </p>
                                 <p>Park Road, USA - 215431</p>
@@ -24,13 +23,12 @@ export default class Footer extends Component {
                     <div class="row d-flex justify-content-between align-items-center">
                         <div class="col-xl-9 col-lg-8">
                             <div class="footer-copy-right">
-                                <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                                <p>
   Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+  </p>
                             </div>
                         </div>
                         <div class="col-xl-3 col-lg-4">
-                            <!-- Footer Social -->
                             <div class="footer-social f-right">
                                 <a>Stay Connected</a>
                                 <a href="#"><i class="fab fa-twitter"></i></a>
@@ -43,7 +41,6 @@ export default class Footer extends Component {
                </div>
             </div>
         </div>
-        <!-- Footer End-->
     </footer>
       </div>
     )
