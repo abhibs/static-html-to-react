75590b9572b54f1e16d03e4928c683beadc1628c
abhibs <abhirambs97@gmail.com>
2025-02-06 12:45:43 +0530

    br closing tag added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -56,7 +56,7 @@ class Home extends Component {
             </div>
             <div class="col-lg-5 offset-lg-1">
                 <div class="about-caption2">
-                    <h3>Any Type Of Query<br> & Discussion.</h3>
+                    <h3>Any Type Of Query<br/> & Discussion.</h3>
                     <p>Late talk with me</p>
                    <div class="send-cv">
                         <a href="#">hire@colorlib.com</a>
